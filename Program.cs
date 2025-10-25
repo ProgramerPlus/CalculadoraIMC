8bc10b9c59c5951fa324909038b68ede0a300516
ProgramerPlus <hipitelo@outlook.com>
2025-10-24 22:12:42 -0300

    Exercício finalizado

---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿double peso;
+double altura;
+double imc;
+
+
+string diagnostico;
+
+
+Console.WriteLine("-- Calculadora de IMC --\n");
+
+
+Console.Write("Digite seu peso em kg...: ");
+peso = Convert.ToDouble(Console.ReadLine());
+
+
+Console.Write("Digite sua altura em m..: ");
+altura = Convert.ToDouble(Console.ReadLine());
+
+
+imc = peso / Math.Pow(altura, 2);
+//ou
+//imc = peso / (altura * altura);
+
+
+//Definir diagnóstico
+if (imc < 17)
+{
+    diagnostico = "Muito abaixo do peso";
+
+}
+else if (imc >= 17 && imc <= 18.49)
+{
+diagnostico = "Abaixo do peso";
+
+}else if(imc>=18.5 && imc <=24.99){
+    
+diagnostico = "Peso normal";
+}else if (imc>=25 && imc<=29.99)
+{
+    diagnostico = "Acima do peso";
+}else if (imc>=30 && imc<=34.99)
+{
+    diagnostico = "Obesidade I";
+}else if (imc>=35 && imc<=39.99)
+{
+diagnostico = "Obesidade II(severa)";
+}
+else 
+{
+    diagnostico = "Obesidade III(mórbida)";
+}
+
+
+
+Console.WriteLine($"\nSeu IMC é {imc:N2} kg/m².");
+Console.WriteLine($"Diagnóstico: {diagnostico}");
+
